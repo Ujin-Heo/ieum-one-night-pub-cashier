25afc1225629021d4b684acea6a238a72dc43aaa
howtojin <hujin2005@korea.ac.kr>
2025-09-10 17:54:31 +0900

    change table num

--- src/BottomTableSection.jsx
+++ src/BottomTableSection.jsx
@@ -10,12 +10,16 @@ function BottomTableSection({ tables, updateCallback: updateCallbackProp }) {
                 <CustomerTable tableInfo={tables[29]} tableNum={29} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group-horizontal">
-                <CustomerTable tableInfo={tables[30]} tableNum={30} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[31]} tableNum={31} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[18]} tableNum={18} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[23]} tableNum={23} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group-horizontal">
-                <CustomerTable tableInfo={tables[32]} tableNum={32} isShort={true} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[33]} tableNum={33} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[25]} tableNum={25} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[24]} tableNum={24} updateCallback={updateCallbackProp} />
+            </div>
+            <div className="table-group-horizontal">
+                <CustomerTable tableInfo={tables[27]} tableNum={27} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[999]} tableNum={999} updateCallback={updateCallbackProp} />
             </div>
         </div>
     )
--- src/TopTableSection.jsx
+++ src/TopTableSection.jsx
@@ -10,27 +10,36 @@ function TopTableSection({ tables, updateCallback: updateCallbackProp }) {
             </div>
             <div className="table-group">
                 <CustomerTable tableInfo={tables[2]} tableNum={2} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[3]} tableNum={999} updateCallback={updateCallbackProp} />
+            </div>
+            <div className="table-group">
                 <CustomerTable tableInfo={tables[3]} tableNum={3} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
-                <CustomerTable tableInfo={tables[4]} tableNum={4} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[4]} tableNum={4} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[29]} tableNum={29} isShort={true} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
-                <CustomerTable tableInfo={tables[5]} tableNum={5} isShort={true} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[6]} tableNum={6} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[5]} tableNum={5} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
-                <CustomerTable tableInfo={tables[7]} tableNum={7} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[6]} tableNum={6} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[30]} tableNum={30} isShort={true} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
-                <CustomerTable tableInfo={tables[8]} tableNum={8} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[7]} tableNum={7} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[8]} tableNum={8} isShort={true} updateCallback={updateCallbackProp} />
                 <CustomerTable tableInfo={tables[9]} tableNum={9} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[999]} tableNum={999} isShort={true} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
-                <CustomerTable tableInfo={tables[10]} tableNum={10} isShort={true} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[11]} tableNum={11} isShort={true} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[12]} tableNum={12} isShort={true} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[13]} tableNum={13} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[10]} tableNum={10} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[11]} tableNum={11} updateCallback={updateCallbackProp} />
+            </div>
+            <div className="table-group">
+                <CustomerTable tableInfo={tables[12]} tableNum={12} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[13]} tableNum={13} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[999]} tableNum={999} isShort={true} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
                 <CustomerTable tableInfo={tables[14]} tableNum={14} updateCallback={updateCallbackProp} />
@@ -39,7 +48,7 @@ function TopTableSection({ tables, updateCallback: updateCallbackProp }) {
             <div className="table-group">
                 <CustomerTable tableInfo={tables[16]} tableNum={16} updateCallback={updateCallbackProp} />
                 <CustomerTable tableInfo={tables[17]} tableNum={17} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[18]} tableNum={18} isShort={true} updateCallback={updateCallbackProp} />
+                <CustomerTable tableInfo={tables[999]} tableNum={999} isShort={true} updateCallback={updateCallbackProp} />
             </div>
             <div className="table-group">
                 <CustomerTable tableInfo={tables[19]} tableNum={19} updateCallback={updateCallbackProp} />
@@ -48,15 +57,6 @@ function TopTableSection({ tables, updateCallback: updateCallbackProp }) {
             <div className="table-group">
                 <CustomerTable tableInfo={tables[21]} tableNum={21} updateCallback={updateCallbackProp} />
                 <CustomerTable tableInfo={tables[22]} tableNum={22} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[23]} tableNum={23} isShort={true} updateCallback={updateCallbackProp} />
-            </div>
-            <div className="table-group">
-                <CustomerTable tableInfo={tables[24]} tableNum={24} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[25]} tableNum={25} updateCallback={updateCallbackProp} />
-            </div>
-            <div className="table-group">
-                <CustomerTable tableInfo={tables[26]} tableNum={26} updateCallback={updateCallbackProp} />
-                <CustomerTable tableInfo={tables[27]} tableNum={27} updateCallback={updateCallbackProp} />
             </div>
         </div>
     )
